84b7384e03ab21b31775f473e15e1f453e42e40e
martinteoharov <martin.teoharov15@gmail.com>
2020-01-19 16:45:16 +0200

    .

--- static/js/qrcode.js
+++ static/js/qrcode.js
@@ -6,7 +6,7 @@ const display = document.getElementById('display');
 
 const result = (id) => {
 	console.log(id);
-	window.location = 'qrate.ddns.net/exponent?id=' + id;
+	window.location = '/exponent?id=' + id;
 }
 
 button.onclick = () => {
@@ -26,7 +26,7 @@ button.onclick = () => {
 			video.style.display = '';
 			video.srcObject = stream;
 			EPPZScrollTo.scrollVerticalToElementById('scanner', 0);
-			const scanner = new QrScanner(video, id => result(id), 720);
+			const scanner = new QrScanner(video, id => result(id), 400);
 			scanner.start();
 		})
 		.catch(error => {
