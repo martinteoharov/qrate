2acc2daa26a8620752e2ef6fb936b6e1437e3c14
martinteoharov <martin.teoharov15@gmail.com>
2020-03-06 15:50:26 +0200

    fuck achoooo

--- static/js/qrcode.js
+++ static/js/qrcode.js
@@ -1,55 +1,26 @@
-const video   = document.getElementById('scanner');
-const button  = document.getElementById('scanQR');
+import QrScanner from "../qr-scanner.min.js";
+
+const upfile  = document.getElementById('upfile')
+const image  = document.getElementById('qrcode');
 const display = document.getElementById('display');
 
 const result = (link) => {
-	console.log(link);
-	window.location.replace(link);
+        console.log(link);
+        window.location.replace(link);
 }
 
-button.onclick = () => {
-	console.log('scanQR:');
-	const videoConstraints = {
-		facingMode:'environment'
-	};
-
-	const constraints = {
-		audio: false
-	};
-
-	navigator.mediaDevices
-		.getUserMedia(constraints)
-		.then(stream => {
-			video.style.display = '';
-			video.srcObject = stream;
-			EPPZScrollTo.scrollVerticalToElementById('scanner', 0);
-
-			//Using instascan now - very nice
-			const scanner = new Instascan.Scanner({ 'video': video, 'mirror': false, 'facingMode':'environment' });
-			scanner.addListener('scan', link => result(link));
-			Instascan.Camera.getCameras().then((cameras) => {
-				if (cameras.length > 0) {
-					let selectedCam = cameras[0];
-					$.each(cameras, (i, c) => {
-						if (c.name.indexOf('back') != -1) {
-							selectedCam = c;
-							return false;
-						}
-					});
-
-					scanner.start(selectedCam);
-				} else {
-					console.error('No cameras found.');
-				}
-			}).catch((e) => {
-				console.error(e);
-			});
-			
-			//Old scanner - very shite
-			//const scanner = new QrScanner(video, link => result(link), 720);
-			//scanner.start();
-		})
-		.catch(error => {
-			console.error(error);
-		});
+image.onclick = () => {
+        document.getElementById("upfile").click();
 }
+
+upfile.onchange = (obj) => {
+        var file = upfile.value;
+        var fileName = file.split("\\");
+        document.getElementById("qrcode").innerHTML = fileName[fileName.length - 1];
+        event.preventDefault();
+
+        QrScanner.scanImage(file)
+                .then(link => result(link))
+                .catch(error => console.log(error || 'No QR code found.'));
+}
+
