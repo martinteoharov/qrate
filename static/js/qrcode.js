7ca331f66aa8777be2c19a8fb8800945eb1365f2
martinteoharov <martin.teoharov15@gmail.com>
2020-03-07 12:29:25 +0200

    2 scanning modes

--- static/js/qrcode.js
+++ static/js/qrcode.js
@@ -1,22 +1,57 @@
-const upfile  = document.getElementById('capture')
-const image  = document.getElementById('qrcode');
-const display = document.getElementById('display');
+import QrScanner from "../qr-scanner.min.js";
+
+const upfileBtn = document.getElementById('capture')
+const imageBtn  = document.getElementById('qrcode');
+const videoBtn  = document.getElementById('qrcode-vid')
+const videoDom  = document.getElementById('scanner')
+const display   = document.getElementById('display');
 
 const result = (link) => {
         console.log(link);
         window.location.replace(link);
 }
-image.onclick = () => {
-	upfile.click();
+
+imageBtn.onclick = () => {
+	upfileBtn.click();
+}
+
+videoBtn.onclick = () => {
+	console.log('video scan:');
+	const videoConstraints = {
+		facingMode:'environment'
+	};
+
+	const constraints = {
+		video: videoConstraints,
+		audio: false
+	};
+
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(stream => {
+			videoDom.style.display = '';
+			videoDom.srcObject = stream;
+
+			EPPZScrollTo.scrollVerticalToElementById('scanner', 0);
+			const scanner = new QrScanner(videoDom, result => display.innerText = result, 480);
+			scanner.start();
+		})
+		.catch(error => {
+			console.error(error);
+		});
+
+	setTimeout( () => newNoty('error', 'Try image scan'), 10000);
+
 }
 
 const openQRCamera = (node) => {
+	console.log('image scan:');
 	let reader = new FileReader();
 	reader.onload = () => {
 		node.value = "";
 		qrcode.callback = (res) => {
 			if(res instanceof Error) {
-				newNoty('error', "No QR code found. Please move farther away from the subject.'");
+				newNoty('error', "No QR code found. Please move farther away from the subject. (50cm away)'");
 			} else {
 				newNoty('success', "Redirecting...");
 				result(res);
@@ -26,4 +61,3 @@ const openQRCamera = (node) => {
 	};
 	reader.readAsDataURL(node.files[0]);
 }
-
